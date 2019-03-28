f3a3695cc27b07662dd8fd74914c4cc6760bf27e
MrHidalgo <Luckyhidalgo@gmail.com>
2019-03-28 12:07:30 +0200

    Initial commit. Create footer - w\ adaptive.

---
+++ src/js/_lib/webFontLoader.js
@@ -0,0 +1,33 @@
+
+
+/**
+ * @name initWebFontLoader
+ *
+ * @description Loading fonts regardless of the source, then adds a standard set of events you may use to control the loading experience... for more details => https://github.com/typekit/fvd
+ */
+const initWebFontLoader = () => {
+
+	/**
+   * @description
+	 */
+	WebFont.load({
+    google: {
+      families: [
+        'PT+Sans:400,700',
+        'PT+Serif:400,700'
+      ]
+    }
+  });
+
+	/**
+   * @description
+	 */
+  // const WebFontConfig = {
+  //   custom: {
+  //     families: [
+  //       'Lato:n1,n3,n4,n5,n6,n7,n9'
+  //     ]
+  //   }
+  // };
+
+};
